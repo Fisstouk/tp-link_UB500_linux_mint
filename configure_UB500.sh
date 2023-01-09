40cbf67e83ad161505b89e4087c3ecfa04ad5b5e
Fisstouk <lyronn.levy@gmail.com>
2023-01-09 23:45:55 +0100

    Download and install drivers

---
+++ configure_UB500.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -x
+set -e
+
+secureboot_test="mokutil --sb-state"
+
+if [ $(secureboot_test) = "SecureBoot disabled" ] ;
+then
+	echo "Script will run..."
+else
+	echo "It is recommended to disable SecureBoot. Do you want to continue?"
+	read answer	
+	if [[ "$answer" = "n" || "$answer" = "no" || "$answer" = "No" ]] ;
+	then
+		exit 0
+	fi	
+
+
+sudo apt install git dkms mokutil
+cd /home/$USER
+git clone https://github.com/jeremyb31/bluetooth-5.15.git
+cd bluetooth-5.15
+make
+sudo mv /lib/modules/$(uname -r)/kernel/drivers/bluetooth/btusb.ko /lib/modules/$(uname -r)/kernel/drivers/bluetooth/btusb.ko.bak
+sudo cp btusb.ko /lib/modules/$(uname -r)/kernel/drivers/bluetooth/
+sudo depmod -a
+
+# Install firmware rtl8761bu_config.bin and rtl8761bu_fw.bin
+cd /lib/firmware/rtl_bt
+sudo wget https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/plain/rtl_bt/rtl8761bu_config.bin
+sudo wget https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/plain/rtl_bt/rtl8761bu_fw.bin
